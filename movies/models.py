723d53864c9ce8f099c10fc7fe398019b1f74e26
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-10-07 09:47:27 +0900

    index add

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=30)
+    grade = models.IntegerField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now = True)
