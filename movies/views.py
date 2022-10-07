fd57a917ee436892107d9172f385ef166b5fe0d1
jiyeong99 <1999wldud@gmail.com>
2022-10-07 10:21:26 +0900

    jy

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import ReviewForm
+from .models import Review
 
 def index(request):
-    return render(request, 'movies/index.html')
+    reviews = Review.objects.all()
+    context = {
+        'reviews' : reviews,
+    }
+    return render(request, 'movies/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            review_form.save()
+            return redirect('movies:index')
+    else :
+        review_form = ReviewForm()
+    context = {
+        'review_form':review_form,
+    }
+    return render(request, 'movies/create.html', context)
